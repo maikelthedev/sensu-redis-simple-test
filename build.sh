34b03820ef46891f350d94cc289e5cb53c18927d
maikel <maikel@maikel.uk>
2021-04-23 17:28:38 +0200

    Added bonsai.yaml

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-PYTHONOPTIMIZE=2 pyinstaller check.py --clean -F --noconfirm
+PYTHONOPTIMIZE=2 pyinstaller check.py --clean -F --noconfirm --distpath sensu-redis-simple-test/bin
 rm build -r
 rm __pycache__ -r
 rm check.spec
+chmod +x sensu-redis-simple-test/bin/*
+tar -C sensu-redis-simple-test -cvzf sensu-redis-simple-test-0.0.1.tar.gz .
+sha512sum  sensu-redis-simple-test-0.0.1.tar.gz | tee sha512sum.txt
