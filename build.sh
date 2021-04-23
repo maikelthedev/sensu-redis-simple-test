73c5a945c56db398a663d8a1e3d741d1e2f61564
maikel <maikel@maikel.uk>
2021-04-23 17:48:00 +0200

    WIP

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ rm __pycache__ -r
 rm check.spec
 chmod +x sensu-redis-simple-test/bin/*
 tar -C sensu-redis-simple-test -cvzf sensu-redis-simple-test-0.0.2.tar.gz .
-sha512sum  sensu-redis-simple-test-0.0.2.tar.gz | tee sensu-redis-simple-test-0.0.2.tar.gz-sha512sum.txt
+sha512sum  sensu-redis-simple-test-0.0.2.tar.gz | tee sensu-redis-simple-test-0.0.2-sha512-checksums.txt
