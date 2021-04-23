473d1c2958fe53ae61db89d1ad70e99608bf986c
maikel <maikel@maikel.uk>
2021-04-23 17:41:57 +0200

    WIP

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-PYTHONOPTIMIZE=2 pyinstaller check.py --clean -F --noconfirm --distpath sensu-redis-simple-test/bin
+PYTHONOPTIMIZE=2 pyinstaller check_redis.py --clean -F --noconfirm --distpath sensu-redis-simple-test/bin
 rm build -r
 rm __pycache__ -r
 rm check.spec
 chmod +x sensu-redis-simple-test/bin/*
-tar -C sensu-redis-simple-test -cvzf sensu-redis-simple-test-0.0.1.tar.gz .
-sha512sum  sensu-redis-simple-test-0.0.1.tar.gz | tee sha512sum.txt
+tar -C sensu-redis-simple-test -cvzf sensu-redis-simple-test-0.0.2.tar.gz .
+sha512sum  sensu-redis-simple-test-0.0.2.tar.gz | tee sensu-redis-simple-test-0.0.2.tar.gz-sha512sum.txt
